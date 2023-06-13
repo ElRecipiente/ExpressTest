28a2c6df3decb0073ee91bf38c18d39bcd8ed0bf
ElRecipiente <nicolas.droux.2b@gmail.com>
2023-06-14 01:27:22 +0200

    Express suxx

--- routes/artists.js
+++ routes/artists.js
@@ -14,11 +14,16 @@ router.get('/name/:name', async function (req, res, next) {
   res.send(artists);
 });
 
-router.get('/name', async function (req, res, next) {
-  const artists = await Artist.find({ "name": "John Doe" })
-  res.send(artists);
-});
+// ça marche pas
+router.get('/tarace', async (req, res, next) => {
+  const artistId = '648824f677ad3c5eec0ae37c';
+  const artist = await Artist.findOne({ "_id": artistId })
+  const album = artist.albums[0]
+  res.send(album)
 
+})
+
+// alors que ça, ça marche
 router.get('/:id/albums/:n', async (req, res, next) => {
   const artist = await Artist.findOne({ "_id": req.params.id })
   const album = artist.albums[req.params.n]
@@ -30,11 +35,20 @@ router.get('/:name/albums', async function (req, res, next) {
   res.send(artists);
 });
 
+// ça, ça marche aussi
 router.get('/albums/:n', async function (req, res, next) {
   const y = parseInt(req.params.n)
   const artists = await Artist.find({ "albums.year": { "$gte": y } })
   res.send(artists);
 });
+
+// et ça, pas du tout
+router.get('/filter', async function (req, res, next) {
+  const years = parseInt(req.query.albumAfter)
+  const artists = await Artist.find({ "albums.year": { "$gte": years } })
+  res.send(artists);
+});
+// mais peut être une réponse ici : https://stackoverflow.com/questions/18524125/req-query-and-req-param-in-expressjs
 
 router.get('/:nation', async function (req, res, next) {
   const artists = await Artist.find({ "country": req.params.nation })
@@ -42,10 +56,39 @@ router.get('/:nation', async function (req, res, next) {
 });
 
 
-router.get('/:name/filter', async function (req, res, next) {
-  const artists = await Artist.findOne({ "name": req.params.name })
+router.get('/artists/:id/songs?orderBy=popularity', async function (req, res, next) {
+  const artists = await Artist.findOne({ "_id": req.params.id })
   const filtered = artists.sort(function (a, b) { a - b })
   res.send(filtered);
+});
+
+
+// TEST AVEC GPT
+router.get('/:id/songs', async (req, res) => {
+  const artistId = req.params.id;
+  const orderBy = req.query.orderBy;
+
+  try {
+    // Récupérer l'artiste correspondant à l'ID
+    const artist = await Artist.findById(artistId);
+
+    if (!artist) {
+      return res.status(404).json({ message: 'Artiste non trouvé' });
+    }
+
+    // Récupérer les chansons de l'artiste
+    const songs = artist.albums.flatMap(album => album.tracks);
+
+    // Trier les chansons par popularité (orderBy=popularity)
+    if (orderBy === 'popularity') {
+      songs.sort((a, b) => b.nbHits - a.nbHits);
+    }
+
+    res.json(songs);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ message: 'Erreur serveur' });
+  }
 });
 
 module.exports = router;
